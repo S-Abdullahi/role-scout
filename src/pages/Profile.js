0c4cd7152d6f2ba9e44a0353d6e78fea92d2b21b
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-06-18 17:19:14 +0100

    profile update

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import FormRow from "../components/FormRow";
+import { toast } from "react-toastify";
 
 const Profile = () => {
   const { isLoading, user } = useSelector((store) => store.user);
@@ -11,62 +13,57 @@ const Profile = () => {
   });
   const dispatch = useDispatch();
 
-  function handleChange (e){
-    const name = e.target.name
-    const value = e.target.value
-    setProfileData({...profileData, [name]: value})
+  function handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    setProfileData({ ...profileData, [name]: value });
   }
 
-  console.log(profileData)
+  function handleSubmit() {
+    const { name, lastName, email, location } = profileData;
+    if (!name || !lastName || !email || !location) {
+      toast.error("please fill out all fields...");
+      return;
+    }
+    return profileData
+  }
+
+  console.log(profileData);
   return (
     <React.Fragment>
       <div className="bg-[--bg-card] rounded mt-10 px-4 py-10  lg:p-10">
         <h2 className="text-2xl mb-4">Profile</h2>
         <form className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          <div className="flex flex-col">
-            <label className="mb-1">Name</label>
-            <input
-              type="text"
-              name="name"
-              labelText="name"
-              value={profileData.name}
-              onChange={handleChange}
-              className="input-field"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label className="mb-1">Last Name</label>
-            <input
-              type="text"
-              name="lastName"
-              labelText="Last Name"
-              value={profileData.lastName}
-              onChange={handleChange}
-              className="input-field"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label className="mb-1">Email</label>
-            <input
-              type="email"
-              name="location"
-              labelText="email"
-              value={profileData.email}
-              onChange={handleChange}
-              className="input-field"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label className="mb-1">Location</label>
-            <input
-              type="text"
-              name="location"
-              labelText="location"
-              value={profileData.location}
-              onChange={handleChange}
-              className="input-field"
-            />
-          </div>
+          <FormRow
+            type="next"
+            name="name"
+            labelText="name"
+            onChange={handleChange}
+            value={profileData.name}
+          />
+          <FormRow
+            type="text"
+            name="lastName"
+            labelText="Last Name"
+            value={profileData.lastName}
+            onChange={handleChange}
+          />
+
+          <FormRow
+            type="email"
+            name="email"
+            labelText="email"
+            value={profileData.email}
+            onChange={profileData.email}
+          />
+
+          <FormRow
+            type="text"
+            name="location"
+            labelText="location"
+            value={profileData.location}
+            onChange={handleChange}
+          />
           <button className="submit-button mt-6">Submit</button>
         </form>
       </div>
