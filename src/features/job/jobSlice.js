77dfc49107064416ea4abc9cfd4e9931259fa106
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-06-21 14:54:10 +0100

    add job integration

--- src/features/job/jobSlice.js
+++ src/features/job/jobSlice.js
@@ -1,14 +1,60 @@
+import { toast } from "react-toastify";
 import customFetch from "../../utils/axios";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
-    isLoading: false,
-}
+  isLoading: false,
+  position: "",
+  company: "",
+  location: "",
+  status: "interview",
+  statusOptions: ["interview", "pending", "decline"],
+  jobType: "full-time",
+  jobTypeOptions: ["full-time", "part-time", "remote", "internship"],
+  isEditing: false,
+};
 
+export const addJob = createAsyncThunk("job/addJob", async (job, thunkAPI) => {
+  console.log("upper testing");
+  try {
+    const resp = await customFetch.post("/jobs", job, {
+      headers: {
+        authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+      },
+    });
+    console.log("testing");
+    return resp.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response);
+  }
+});
 
 const jobSlice = createSlice({
-    name: 'job',
-    initialState
-})
+  name: "job",
+  initialState,
+  reducers: {
+    handleInputChange: (state, { payload }) => {
+      const { name, value } = payload;
+      state[name] = value;
+    },
+    handleClear: () => {
+      return { ...initialState };
+    },
+  },
+  extraReducers: {
+    [addJob.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [addJob.fulfilled]: (state) => {
+      state.isLoading = false;
+      toast.success("Job Created");
+    },
+    [addJob.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+  },
+});
 
-export default jobSlice.reducer
+export default jobSlice.reducer;
+export const { handleInputChange, handleClear } = jobSlice.actions;
--- src/pages/AddJobs.js
+++ src/pages/AddJobs.js
@@ -3,6 +3,9 @@ import FormRow from "../components/FormRow";
 import { Form } from "react-router-dom";
 import FormSelect from "../components/FormSelect";
 import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { handleInputChange, handleClear } from "../features/job/jobSlice";
+import { addJob } from "../features/job/jobSlice";
 
 const initialJobData = {
   position: "",
@@ -10,58 +13,68 @@ const initialJobData = {
   location: "",
   status: "",
   jobType: "",
-}
+};
 
 const AddJobs = () => {
-  const statusOptions = ["interview", "pending", "decline"];
-  const jobTypeOption = ["full-time", "part-time", "remote", "internship"];
+  const {
+    isLoading,
+    position,
+    company,
+    status,
+    statusOptions,
+    jobType,
+    jobTypeOptions,
+    isEditing,
+    location,
+  } = useSelector((store) => store.job);
+  const dispatch = useDispatch();
   const [jobData, setJobData] = useState(initialJobData);
 
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    setJobData({ ...jobData, [name]: value });
-    console.log(jobData);
+    dispatch(handleInputChange({ name, value }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const { position, company, location, status, jobType } = jobData;
     if (!position || !company || !location || !status || !jobType) {
       toast.error("please fill a fields");
       return;
     }
+    console.log(position, company, status, location, jobType);
+    dispatch(
+      addJob({ position, company, jobLocation: location, status, jobType })
+    );
   };
-
-  const handleClear = () =>{
-    setJobData(initialJobData)
-    toast.success('All fields cleared')
-  }
 
   return (
     <React.Fragment>
       <div className="bg-[--bg-card] rounded px-4 py-10  lg:p-10">
-        <h2 className="text-2xl mb-4">Add Job</h2>
-        <form className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3" onSubmit={handleSubmit}>
+        <h2 className="text-2xl mb-4">{isEditing ? "Edit Job" : "Add Job"}</h2>
+        <form
+          className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+          onSubmit={handleSubmit}
+        >
           <FormRow
             type="text"
             name="position"
             labelText="Position"
-            value={jobData.position}
+            value={position}
             handleChange={handleChange}
           />
           <FormRow
             type="text"
             name="company"
             labelText="company"
-            value={jobData.company}
+            value={company}
             handleChange={handleChange}
           />
           <FormRow
             type="text"
             name="location"
             labelText="Job Location"
-            value={jobData.location}
+            value={location}
             handleChange={handleChange}
           />
 
@@ -69,23 +82,30 @@ const AddJobs = () => {
             labelText="Status"
             name="status"
             options={statusOptions}
-            value={jobData.status}
+            value={status}
             handleChange={handleChange}
           />
 
           <FormSelect
             labelText="Job Type"
             name="jobType"
-            options={jobTypeOption}
-            value={jobData.jobType}
+            options={jobTypeOptions}
+            value={jobType}
             handleChange={handleChange}
           />
 
           <div className="grid grid-cols-2 gap-3">
-            <button type="submit" className="bg-[--bg-icon] rounded text-[--text-active] mt-7 h-8">
-              Submit
+            <button
+              type="submit"
+              className="bg-[--bg-icon] rounded text-[--text-active] mt-7 h-8"
+            >
+              {isLoading ? "creating job..." : "submit"}
             </button>
-            <button type="reset" className="rounded text-[--text-active] mt-7 h-8 bg-[--bg-main]" onClick={handleClear}>
+            <button
+              type="reset"
+              className="rounded text-[--text-active] mt-7 h-8 bg-[--bg-main]"
+              onClick={() => dispatch(handleClear())}
+            >
               Clear
             </button>
           </div>
